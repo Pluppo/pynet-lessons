b7d911c7128bab018943d7ca0795d68711cc9c81
Pluppo <erik_ingeberg@hotmail.com>
2018-10-10 11:02:55 -0400

    Corrected code

--- examples/gen_dict_extract.py
+++ examples/gen_dict_extract.py
@@ -1,14 +1,14 @@
+#Define function for finding values in recursive dictionaries:
 #Example for printing all values of key 'my_key' in dictionary 'my_dict':
 #for x in gen_dict_extract(my_dict, 'my_key'):
 #    print(x)
-
 def gen_dict_extract(var, key):
      if isinstance(var, dict):
          for k, v in var.items():
              if k == key:
                  yield v
-             if isintance(v, (dict, list)):
+             if isinstance(v, (dict, list)):
                  yield from gen_dict_extract(v, key)
              elif isinstance(var, list):
                  for d in var:
-                     yield from gen_dict_extract(d, kek)
+                     yield from gen_dict_extract(d, key)
